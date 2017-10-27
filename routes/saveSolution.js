7a97d54863ed291f6e5a1343dde0fbb813e020db
bali182 <edesbalazs@gmail.com>
2017-10-27 18:58:09 +0100

    minor update

--- routes/saveSolution.js
+++ routes/saveSolution.js
@@ -1,7 +1,7 @@
 const isArray = require('lodash/isArray')
 const isEqual = require('lodash/isEqual')
-
 const isObject = require('lodash/isObject')
+
 const query = require('../db/query')
 const expected = require('../data/solution.json')
 
@@ -19,19 +19,19 @@ const saveSolution = (req, res) => {
   for (let i = 0; i < solution.length; i++) {
     const entry = solution[i]
     if (!isObject(entry)) {
-      return res.error({
+      return res.json({
         error: `Entry at index ${i} is not an object!`,
         hint: 'Check the task description for more details!'
       })
     }
     if (!isObject(entry.user)) {
-      return res.error({
+      return res.json({
         error: `Value for key 'user' in entry at index ${i} isn't an object!`,
         hint: 'Check the task description for more details!'
       })
     }
     if (!isArray(entry.products)) {
-      return res.error({
+      return res.json({
         error: `Value for key 'products' in entry at index ${i} isn't an array!`,
         hint: 'Check the task description for more details!'
       })
@@ -39,7 +39,7 @@ const saveSolution = (req, res) => {
   }
 
   if (!isEqual(expected, solution)) {
-    return res.error({
+    return res.json({
       error: `Your solution is incorrect, please try again!`,
       hint: 'Check the task description for more details!'
     })
