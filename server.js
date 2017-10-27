26b04842c710b1126fbc49f8faf42403b9a01bd1
bali182 <edesbalazs@gmail.com>
2017-10-27 12:04:47 +0100

    test change

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(req, res) {
+app.set('json spaces', 2)
+
+app.get('/', function (req, res) {
   res.send({
     "Output": "Hello World!"
   });
 });
 
-app.post('/', function(req, res) {
+app.post('/', function (req, res) {
   res.send({
     "Output": "Hello World!"
   });
