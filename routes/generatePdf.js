c5d0497ecbd0378696a67e6b8469cb401764f192
bali182 <edesbalazs@gmail.com>
2017-10-31 15:15:57 +0000

    testing enpointRoot

--- routes/generatePdf.js
+++ routes/generatePdf.js
@@ -2,8 +2,9 @@ const createCandidate = require('./utils/createCandidate')
 const createPdf = require('./utils/createPdf')
 
 const generatePdf = (req, res) => {
+  const endpointRoot = req.app.mountpath || req.baseUrl
   createCandidate(req.query)
-    .then(candidateId => createPdf({ candidateId, endpointRoot: req.baseUrl }))
+    .then(candidateId => createPdf({ candidateId, endpointRoot }))
     .then(buffer => res.end(buffer, 'binary'))
     .catch(error => res.json({
       error: "Couldn't create PDF",
