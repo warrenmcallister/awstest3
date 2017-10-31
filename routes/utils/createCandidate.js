deed50619b82e7eeb8fa976ab2a3d80a182b1681
bali182 <edesbalazs@gmail.com>
2017-10-31 14:57:27 +0000

    fixes id gen

--- routes/utils/createCandidate.js
+++ routes/utils/createCandidate.js
@@ -5,7 +5,7 @@ const createCandidate = ({ name }) => {
   if (!name || name.length === 0) {
     throw new Error("'name' is mandatory")
   }
-  const id = removeAccents(`${name.toLowerCase().replace(/\s\s+/g, '-')}`)
+  const id = removeAccents(name.toString()).toLowerCase().replace(/\s+/g, '-')
   return query('INSERT INTO candidates VALUES (?, ?)', [id, 0]).then(response => id)
 }
 
